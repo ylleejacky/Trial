d6183816ad08c007629847c5a21335ada9a5cb30
ylleejacky <34020378+ylleejacky@users.noreply.github.com>
2017-11-27 14:32:39 +0800

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,7 @@
+# Calculate the total heart beats of a 80-year-old adult.
+
+print ("Assuming the condition that a human heart beat rate is 1 time per sec")
+a= 80 * 365.25 * 24 * 60 * 60 # total heart beats within 80 years.
+print ("The total heart beat of a 80-year-old adult is")
+print (a)
+print ("times.")
